84f7c058a75ffb3880823ee4ca3deccc91b5d52b
Prokrus <binwumm@gmail.com>
2020-05-05 15:27:50 +0800

    remove useless files

--- decision_tree/test/script.py
+++
@@ -1,7 +0,0 @@
-import trees
-
-dataSet, labels = trees.createDataSet()
-shann = trees.calcShannonEnt(dataSet)
-split_result = trees.splitDataSet(dataSet, 1, 0)
-trees.chooseBestFeatureToSplit(dataSet)
-print dataSet
